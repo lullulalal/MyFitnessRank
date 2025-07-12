2e5f45a9a918191f756ef4de2e590f78cdef1d59
Minsu Seo <lullulalal@gmail.com>
2025-07-12 05:13:09 +0200

    Use public url

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
-  static const String baseUrl = 'http://127.0.0.1:8000/myfitnessrank';
-  //static const String baseUrl = 'https://138.199.215.9.nip.io/myfitnessrank';
+  //static const String baseUrl = 'http://127.0.0.1:8000/myfitnessrank';
+  static const String baseUrl = 'https://138.199.215.9.nip.io/myfitnessrank';
 
   static const String running = '$baseUrl/running';
 }
