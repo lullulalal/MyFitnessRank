2ea0380a167d52fd074454578f38efb0b681fdd7
Minsu Seo <lullulalal@gmail.com>
2026-08-09 19:31:56 +0900

    Change baseUrl to dev env's one

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
-  //static const String baseUrl = 'http://127.0.0.1:8000/MyRunRank';
-  static const String baseUrl = 'https://138.199.215.9.nip.io/MyRunRank';
+  static const String baseUrl = 'http://127.0.0.1:8000/MyRunRank';
+  //static const String baseUrl = 'https://138.199.215.9.nip.io/MyRunRank';
 
   static const String running = '$baseUrl/running';
 }
